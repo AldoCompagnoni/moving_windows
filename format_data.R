bc639a714dbb265a6dd7a7f96dfa17bc4d76a3a5
Compagnoni <ac22qawo@idiv.de>
2017-07-06 23:36:54 +0200

    Format clim and lam data

---
+++ format_data.R
@@ -0,0 +1,72 @@
+# species-specific lambda and climate data 
+setwd("C:/cloud/Dropbox/sAPROPOS project/DemogData")
+library(dplyr)
+library(tidyr)
+library(chron)
+
+# read data -----------------------------------------------------
+lam   <- read.csv("lambdas_6tr.csv", stringsAsFactors = F)
+clim  <- read.csv("precip_fc.csv",  stringsAsFactors = F)
+spp   <- clim$species %>% unique
+
+# format species ------------------------------------------------
+
+format_species <- function(spp_name, lam){
+  
+  lam   <- lam %>%
+            subset(SpeciesAuthor == spp_name) %>%
+            dplyr::select(MatrixEndYear, lambda) %>%
+            setNames(c("year","lambda")) %>%
+            mutate(log_lambda = log(lambda))
+  return(lam)
+  
+}
+
+# format climate ------------------------------------------------
+format_climate <- function(spp_name, clim){
+  
+  # select species-specific climate
+  clim_spp <- clim %>% subset(species == spp_name)
+  
+  # format day one
+  day_one  <- as.Date(paste0("1/1/",first(clim_spp$year)), 
+                      format="%d/%m/%Y") 
+  
+  # climate data
+  clim_d   <- as.Date(1:nrow(clim_spp), day_one-1) %>%
+                as.character %>%
+                as.data.frame(stringsAsFactors=F) %>%
+                separate_(col=".",into=c("year1","month1","day1"),sep="-") %>%
+                bind_cols(clim_spp) %>%
+                select(-year,-day) %>%
+                setNames( c("year", "month", "day", "species", "ppt") )
+       
+  # monthly climates
+  clim_m   <- clim_d %>%
+                group_by(year, month) %>%
+                summarise( ppt = sum(ppt) ) %>%
+                spread(month, ppt) %>%
+                setNames( c("year",month.abb))
+   
+  # detrend climate
+  d_prec   <- apply(clim_m[,-1], 2, FUN = scale, center = T, scale = T) %>% 
+                as.data.frame() %>%
+                bind_cols(clim_m[,1]) %>%
+                select( c("year",month.abb) )
+    
+}
+
+
+
+# select species
+spp_dur <- lam %>% 
+            group_by(SpeciesAccepted) %>% 
+            summarise(duration = length(unique(MatrixStartYear)))
+
+# species list
+spp_list<- lam$SpeciesAuthor %>% unique
+  subset(Lat == 38.8 & Lon == -99.2) %>%
+  .[,"SpeciesAccepted"] %>%
+  as.character %>%
+  unique
+
