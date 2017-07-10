552305a938ec02b64bbd7e04fc31896a00c1a176
Compagnoni <ac22qawo@idiv.de>
2017-07-10 19:14:26 +0200

    Format climate at level of 'MaxtrixPopulation'
    Separate 'MatrixPopulation' using lists

--- format_data.R
+++ format_data.R
@@ -6,7 +6,7 @@ library(tidyr)
 
 # read data -----------------------------------------------------
 lam     <- read.csv("lambdas_6tr.csv", stringsAsFactors = F)
-clim    <- read.csv("precip_fc.csv",  stringsAsFactors = F)
+clim    <- read.csv("precip_fc.csv",  stringsAsFactors = F) #clim<-read.csv("precip_fc_demo.csv",  stringsAsFactors = F)
 spp     <- clim$species %>% unique
 m_back  <- 24
 
@@ -16,64 +16,67 @@ format_species <- function(spp_name, lam){
   
   lam   <- lam %>%
             subset(SpeciesAuthor == spp_name) %>%
-            dplyr::select(MatrixEndYear, lambda) %>%
-            setNames(c("year","lambda")) %>%
+            dplyr::select(MatrixEndYear, MatrixPopulation, lambda) %>%
+            setNames(c("year","MatrixPopulation","lambda")) %>%
             mutate(log_lambda = log(lambda))
   return(lam)
   
 }
 
-lam_spp <- format_species(spp_name, lam)
-
-# format climate ------------------------------------------------
+# format climate 
 format_climate <- function(spp_name, clim, lam_spp){
   
   # select species-specific climate
-  clim_spp <- clim %>% subset(species == spp_name)
+  clim_spp  <- clim %>% 
+                  subset(species == spp_name) %>%
+                  mutate( population = as.factor(population) )
+    
+  # list of climate
+  clim_l    <- clim_spp %>%
+                  select(-population) %>%
+                  split( clim_spp$population )
+
+  return(clim_l)
   
+}
+
+# format populations
+format_pop <- function(clim_x){
+
   # format day one
-  day_one  <- as.Date(paste0("1/1/",first(clim_spp$year)), 
-                      format="%d/%m/%Y") 
-  
+  day_one   <- as.Date( paste0("1/1/", first(clim_x$year) ), 
+                        format="%d/%m/%Y") 
+
   # climate data
-  clim_d   <- as.Date(1:nrow(clim_spp), day_one-1) %>%
+  clim_d    <- as.Date(1:nrow(clim_x), day_one-1) %>%
                 as.character %>%
                 as.data.frame(stringsAsFactors=F) %>%
                 separate_(col=".",into=c("year1","month1","day1"),sep="-") %>%
-                bind_cols(clim_spp) %>%
+                bind_cols(clim_x) %>%
                 select(-year,-day) %>%
                 setNames( c("year", "month", "day", "species", "ppt") )
-       
+  
   # monthly climates
   clim_m   <- clim_d %>%
                 group_by(year, month) %>%
-                summarise( ppt = sum(ppt) ) %>%
+                summarise( ppt = sum(ppt, na.rm=T) )  %>%
                 spread(month, ppt) %>%
                 setNames( c("year",month.abb))
-   
+                
   # detrend climate
   d_prec   <- apply(clim_m[,-1], 2, FUN = scale, center = T, scale = T) %>% 
                 as.data.frame() %>%
                 bind_cols(clim_m[,1]) %>%
                 select( c("year",month.abb) )
-    
-  # long form (again!)
-  
-  
+
+  return(d_prec)
   
 }
 
 
+# format by species ----------------------------------------------------------
+spp_name      <- spp[1] # test run w/ spp number 1
 
-# select species
-spp_dur <- lam %>% 
-            group_by(SpeciesAccepted) %>% 
-            summarise(duration = length(unique(MatrixStartYear)))
-
-# species list
-spp_list<- lam$SpeciesAuthor %>% unique
-  subset(Lat == 38.8 & Lon == -99.2) %>%
-  .[,"SpeciesAccepted"] %>%
-  as.character %>%
-  unique
-
+spp_lambdas   <- format_species(spp_name, lam)
+spp_raw_clim  <- format_climate(spp_name, clim, lam_spp)
+spp_clim      <- lapply(spp_raw_clim, format_pop)
