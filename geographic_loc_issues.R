1b512f86cef77cfcd551cba08b7da3612987df7b
Compagnoni <ac22qawo@idiv.de>
2017-08-09 13:19:17 +0200

    Add correlation between monthly climate of sites; Reorder climate code, put all functions first

--- geographic_loc_issues.R
+++ geographic_loc_issues.R
@@ -26,7 +26,24 @@ spp_list  <- lambdas$SpeciesAuthor %>% unique %>% .[-3] # remove Daphne rodrigue
 
 
 
-# mean yearly climate: spp-by-site --------------------------------------------------
+# CLIMATE across sites (yearly means, yearly and monthly correlations) ---------------------------------------------
+
+# identify species with multiple sites 
+spp_mpl_sites <- function(ii){
+  
+  spp_name      <- spp_list[ii] # test run w/ spp number 1
+  m_back        <- 24     # months back
+  expp_beta     <- 20
+  
+  # lambda data
+  spp_lambdas   <- format_species(spp_name, lambdas)
+  
+  data.frame( species = spp_name,
+              site_rep = length(spp_lambdas) )  
+  
+}
+
+# mean yearly climate: spp-by-site 
 spp_by_site_climate <- function(ii){
   
   spp_name      <- spp_list[ii] # test run w/ spp number 1
@@ -54,6 +71,7 @@ spp_by_site_climate <- function(ii){
                                       clim_separate, 
                                       names(clim_separate) 
                        )
+  
   if( length(clim_means_l) > 1 ) {
     
     clim_means <- Reduce(function(...) rbind(...), clim_means_l)
@@ -68,43 +86,9 @@ spp_by_site_climate <- function(ii){
   clim_means
   
 }
-# site level climates for all species 
-site_clim_l     <- lapply(1:length(spp_list), spp_by_site_climate) 
-site_clim_df    <- Reduce(function(...) bind_rows(...), site_clim_l) 
-
-# between species differences in climate
-spp_clims       <- site_clim_df %>% 
-                      group_by( species ) %>%
-                      summarise( mean_ppt = mean( mean_p ) )
-spp_clim_diff   <- diff(spp_clims$mean_ppt) %>% as.matrix
 
 
-
-# identify species with multiple sites ----------------------------------------------
-spp_mpl_sites <- function(ii){
-  
-  spp_name      <- spp_list[ii] # test run w/ spp number 1
-  m_back        <- 24     # months back
-  expp_beta     <- 20
-  
-  # lambda data
-  spp_lambdas   <- format_species(spp_name, lambdas)
-  
-  data.frame( species = spp_name,
-              site_rep = length(spp_lambdas) )  
-  
-}
-
-# spp with multiple site information
-mpl_spp_l   <- lapply(1:length(spp_list), spp_mpl_sites )
-mpl_spp_df  <- Reduce(function(...) rbind(...), mpl_spp_l) %>%
-                  subset( site_rep > 1 )
-
-# site spp_diff
-mpl_site_spp<- mpl_spp_df$species %>% as.character
-
-
-# between site differences in climate --------------------------------------------------
+# between site differences in climate 
 bw_site_clim <- function(ii){
   
   spp_name    <- mpl_site_spp[ii]
@@ -112,30 +96,12 @@ bw_site_clim <- function(ii){
   sites_clim  <- subset(site_clim_df, species %in% spp_name) 
   cl_st_diff  <- data.frame( species = spp_name,
                              site_clim_diff = diff(sites_clim$mean_p) %>% as.matrix %>% as.vector
-                            )
+  )
   
 }
-bw_site_diff_l  <- lapply(1:length(mpl_site_spp), bw_site_clim)
-bw_site_diff    <- Reduce(function(...) rbind(...), bw_site_diff_l) 
 
-# exclude species whose difference in climate is 0 (not a problem)
-probl_spp <- bw_site_diff %>% 
-                group_by( species) %>% 
-                summarise( mean_spp_clim = mean(site_clim_diff) ) %>%
-                ungroup %>%
-                subset( mean_spp_clim != 0 ) %>%
-                as.data.frame %>%
-                .[,"species"] %>%
-                as.character
-
-# Cypripedium_fasciculatum and Brassica_insularis definitely raise a brow!
-# Astragalus_scaphoides_2, Trillium_ovatum, 
-subset(bw_site_diff, species %in% probl_spp )
-
-
-
-# correlation of monthly climates between sites
-site_clim_corr <- function(spp_name){
+# correlation of YEARLY climates between sites 
+site_yr_clim_corr <- function(spp_name){
   
   #spp_name      <- spp_list[ii] # test run w/ spp number 1
   m_back        <- 24     # months back
@@ -158,19 +124,128 @@ site_clim_corr <- function(spp_name){
   site_yr_clim_l    <- lapply(clim_sep_mean, function(x) x$ppt )
   site_yr_clim_mat  <- do.call(cbind, site_yr_clim_l)
   cor_mat           <- cor(site_yr_clim_mat) 
-
+  
   cor_mat[ upper.tri(cor_mat) ]
   
 }
 
+# correlation of MONTHLY climates between sites 
+site_mon_clim_corr <- function(spp_name){
+  
+  #spp_name      <- spp_list[ii] # test run w/ spp number 1
+  m_back        <- 24     # months back
+  expp_beta     <- 20
+  
+  # lambda data
+  spp_lambdas   <- format_species(spp_name, lambdas)
+  
+  # climate daily data
+  clim_separate <- clim_list(spp_name, clim, spp_lambdas)
+  
+  
+  # extractly monthly climate population-level climate; put it in "long" form
+  clim_month <- function(clim_x, clim_var = "precip"){ #, pops
+    
+    # format day one
+    day_one   <- as.Date( paste0("1/1/", first(clim_x$year) ), 
+                          format="%d/%m/%Y")
+    
+    # climate data
+    clim_d    <- as.Date(1:nrow(clim_x), day_one-1) %>%
+      as.character %>%
+      as.data.frame(stringsAsFactors=F) %>%
+      separate_(col=".",into=c("year1","month1","day1"),sep="-") %>%
+      bind_cols(clim_x) %>%
+      dplyr::select(-year,-day) %>%
+      setNames( c("year", "month", "day", "species", "ppt") )
+    
+    # monthly climates
+    # if climate var relate to precipitation, monthly SUMS 
+    if( clim_var == "precip" | clim_var == "pet"){
+      clim_m   <- clim_d %>%
+        group_by(year, month) %>%
+        summarise( ppt = sum(ppt, na.rm=T) )  %>%
+        as.data.frame() #%>%
+      #add_column(population = pops, .after = 1)
+    }
+    #
+    if( clim_var == "airt"){
+      clim_m   <- clim_d %>%
+        group_by(year, month) %>%
+        summarise( ppt = mean(ppt, na.rm=T) )  %>%
+        as.data.frame()
+    }  
+    # throw error
+    if( !any( clim_var %in% c("precip","pet","airt")) ) {
+      stop( paste0(clim_var," is not a supported varible") ) 
+    }
+    
+    return(clim_m)
+    
+  }
+  
+  # monthly climate
+  clim_m_l    <- lapply(clim_separate, clim_month)
+  clim_m_m_l  <- lapply(clim_m_l, function(x) x$ppt)
+  clim_m      <- do.call(cbind, clim_m_m_l)
+  
+  # get correlation of climates across sites 
+  cor_mat           <- cor(clim_m) 
+  
+  cor_mat[ upper.tri(cor_mat) ]
+  
+}
+
+# spp with multiple site information
+mpl_spp_l   <- lapply(1:length(spp_list), spp_mpl_sites )
+mpl_spp_df  <- Reduce(function(...) rbind(...), mpl_spp_l) %>%
+                  subset( site_rep > 1 )
+# site spp_diff
+mpl_site_spp<- mpl_spp_df$species %>% as.character
+
+
+# site level climates for all species 
+site_clim_l     <- lapply(1:length(spp_list), spp_by_site_climate) 
+site_clim_df    <- Reduce(function(...) bind_rows(...), site_clim_l) 
+
+# between species differences in climate
+spp_clims       <- site_clim_df %>% 
+                      group_by( species ) %>%
+                      summarise( mean_ppt = mean( mean_p ) )
+spp_clim_diff   <- diff(spp_clims$mean_ppt) %>% as.matrix
+
+
+# between site differences in climate
+bw_site_diff_l  <- lapply(1:length(mpl_site_spp), bw_site_clim)
+bw_site_diff    <- Reduce(function(...) rbind(...), bw_site_diff_l) 
+
+# exclude species whose difference in climate is 0 (not a problem)
+probl_spp <- bw_site_diff %>% 
+                group_by( species) %>% 
+                summarise( mean_spp_clim = mean(site_clim_diff) ) %>%
+                ungroup %>%
+                subset( mean_spp_clim != 0 ) %>%
+                as.data.frame %>%
+                .[,"species"] %>%
+                as.character
+
+# Cypripedium_fasciculatum and Brassica_insularis definitely raise a brow!
+# Astragalus_scaphoides_2, Trillium_ovatum, 
+subset(bw_site_diff, species %in% probl_spp )
+
+
 # correlation of yearly climates
-clim_corr_l <- lapply(mpl_site_spp, site_clim_corr) %>%
+clim_corr_l <- lapply(mpl_site_spp, site_yr_clim_corr) %>%
+                    setNames(mpl_site_spp)
+
+# correlation of monthly climates
+clim_m_corr_l <- lapply(mpl_site_spp, site_mon_clim_corr) %>%
                     setNames(mpl_site_spp)
 
 
 
 
-# eucledian geographic distance
+# eucledian geographic distance --------------------------------------------------------------
 euclid_dist <- function(ii){
   
   spp_name    <- probl_spp[ii]
