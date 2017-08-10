3c426484e9e9dcf26b13e3046302d2ff7c3cd0ad
Compagnoni <ac22qawo@idiv.de>
2017-08-10 19:30:38 +0200

    Add prediction of mean climate of past 24/12 months

--- splines.R
+++ splines.R
@@ -4,17 +4,19 @@ source("~/moving_windows/format_data.R")
 library(tidyverse)
 library(dismo)
 library(mgcv)
-library(testhtat)
+library(testthat)
 
 # climate predictor, months back, max. number of knots
-clim_var  <- "pet"
-m_back    <- 24    
+rm(list=ls())
+source("C:/CODE/moving_windows/format_data.R")
+clim_var  <- "precip"
+m_back    <- 12    
 knots     <- 8
 
 # read data -----------------------------------------------------------------------------------------
 lam       <- read.csv("lambdas_6tr.csv", stringsAsFactors = F) 
 m_info    <- read.csv("MatrixEndMonth_information.csv", stringsAsFactors = F)
-clim      <- read.csv(paste0(clim_var,"_fc_demo.csv"),  stringsAsFactors = F)
+clim      <- data.table::fread(paste0(clim_var,"_fc_demo.csv"),  stringsAsFactors = F)
 spp       <- clim$species %>% unique
 
 
@@ -54,17 +56,18 @@ for(ii in 1:length(spp_list)){
   expect_equal(length(spp_lambdas), length(clim_separate) )
   
   # unique years, the basis of crossvalidation samples
-  unique_yr         <- arrange(mod_data$lambdas,year) %>% .$year %>% unique
+  unique_yr         <- mod_data$lambdas$year %>% unique
   
   # format data for splines --------------------------------------------------------
   
   # precipitation matrix
   pmat      <- mod_data$climate %>% setNames(NULL) %>% as.matrix
+  pmean     <- apply(pmat, 1, mean)
   
   # set up lags
-  lags      <- matrix(0, nrow(mod_data$climate), ncol(mod_data$climate)); 
-  for(i in 1:ncol(lags)) lags[,i]=i; 
-  lagsm     <- as.matrix(lags); 
+  lags      <- matrix(0, nrow(mod_data$climate), ncol(mod_data$climate)) 
+  for(i in 1:ncol(lags)) lags[,i]=i
+  lagsm     <- as.matrix(lags)
   
   # assemble final data frame
   # pmat <- pmat/diff(range(pmat));
@@ -72,7 +75,7 @@ for(ii in 1:length(spp_list)){
   dat       <- as.data.frame(dat, colnames=TRUE)
   dat$lags  <- lags
   dat$pmat  <- pmat
-  
+  dat$pmean <- pmean
   
   # model fits -----------------------------------------------------------------------------------
   
@@ -94,39 +97,51 @@ for(ii in 1:length(spp_list)){
     # train and test sets, null prediction
     train_set   <- subset( dat, year != yr )
     test_set    <- subset( dat, year == yr )
-    pred_null   <- mean( train_set$log_lambda )
     
-    # model: leave-one-out
+    # model: leave-one-year-out
     if( length(unique(dat$population)) > 1 ){
-      mod_loo     <- gam(log_lambda ~ s(lags, k=knots, by=pmat, bs="cs",sp=mod_full$sp),# + population,
+      mod_spline  <- gam(log_lambda ~ s(lags, k=knots, by=pmat, bs="cs",sp=mod_full$sp),# + population,
                          method="GCV.Cp",gamma=1.4, data=train_set)
+      mod_lm      <- lm(log_lambda ~ pmean, data=train_set)# + population
     }else{
-      mod_loo     <- gam(log_lambda ~ s(lags, k=knots, by=pmat, bs="cs",sp=mod_full$sp),
+      mod_spline  <- gam(log_lambda ~ s(lags, k=knots, by=pmat, bs="cs",sp=mod_full$sp),
                          method="GCV.Cp",gamma=1.4, data=train_set)
+      mod_lm      <- lm(log_lambda ~ pmean, data=train_set)
     }
     
+    # predictions leave-one-out: spline, linear model
+    pred_null   <- mean( train_set$log_lambda )
+    pred_lm     <- predict(mod_lm,     newdata = test_set, type="response")
+    pred_spline <- predict(mod_spline, newdata = test_set, type="response")
     
-    # prediction: leave-one-out
-    pred_oo     <- predict(mod_loo, newdata = test_set, type="response")
+    # data frame of predictions and "design" (year-by-population)
+    pred_df     <- data.frame(pred_null   = pred_null, 
+                              pred_lm     = pred_lm,
+                              pred_spline = pred_spline,
+                              stringsAsFactors = F
+                              )
+    design_pred <- dplyr::select(test_set, year, population)
     
-    data.frame( pred_null = pred_null, 
-                pred_oo   = pred_oo )
+    bind_cols(design_pred, pred_df) 
     
   }
   
   # apply function across samples, and create data frame
   crxval_l    <- lapply(1:length(unique_yr), crxval_spline, dat, pmat)
-  crxval_df   <- Reduce(function(...) rbind(...), crxval_l)
+  crxval_df   <- Reduce(function(...) rbind(...), crxval_l) %>%
+                    left_join( dplyr::select(dat,year, population,log_lambda) )
   
   # Mean squared error
-  dev0        <- calc.deviance(crxval_df$pred_null, dat$log_lambda, 
+  dev0        <- calc.deviance(crxval_df$log_lambda, crxval_df$pred_null, 
                                weights = rep(1, nrow(crxval_df) ),  
                                family="gaussian", calc.mean = TRUE)  
-  dev1        <- calc.deviance(crxval_df$pred_oo, dat$log_lambda, 
+  dev1        <- calc.deviance(crxval_df$log_lambda, crxval_df$pred_lm, 
+                               weights = rep(1, nrow(crxval_df) ),  
+                               family="gaussian", calc.mean = TRUE)
+  dev2        <- calc.deviance(crxval_df$log_lambda, crxval_df$pred_spline,
                                weights = rep(1, nrow(crxval_df) ),  
                                family="gaussian", calc.mean = TRUE)
   
- 
   # plot results ---------------------------------------------------------------
   tiff(paste0("results/splines/loyo/plots/",clim_var,m_back,"/",spp_name,".tiff"),
        unit="in", width=6.3, height=3.15, res=400, compression="lzw")
@@ -138,45 +153,53 @@ for(ii in 1:length(spp_list)){
   abline(h=0)
   
   # plot populations separately
-  pops <- dat$population %>% unique
+  pops <- crxval_df$population %>% unique
   
   # all lambdas (for limits of y-axis)
-  all_llam  <- c(dat$log_lambda, crxval_df$pred_null, crxval_df$pred_oo) 
+  all_llam  <- c(crxval_df$log_lambda, 
+                 crxval_df$pred_null, 
+                 crxval_df$pred_lm, 
+                 crxval_df$pred_spline) 
   
   # graph lambdas 
   for(p in 1:length(pops)){
     
     # population index
-    pop_i     <- which(dat$population == pops[p])
+    pop_i       <- which(crxval_df$population == pops[p])
     
     # prepare data for plotting
-    plot_data <- dat[pop_i,]
-    plot_null <- crxval_df$pred_null[pop_i]
-    plot_pred <- crxval_df$pred_oo[pop_i]
+    plot_data   <- crxval_df[pop_i,]
+    plot_null   <- plot_data$pred_null[pop_i]
+    plot_lm     <- plot_data$pred_lm[pop_i]
+    plot_spline <- plot_data$pred_spline[pop_i]
     
     if( p == 1){
-      plot(plot_data$log_lambda ~ plot_data$year, type="o", ylim = range(all_llam),
-           ylab="logLambda", xlab="Left-out year", col="black")
+      plot(log_lambda ~ year, type="o", ylim = range(all_llam),
+           ylab="logLambda", xlab="Left-out year", col="black", data = plot_data)
       
     } else{
-      points(plot_data$log_lambda ~ plot_data$year, type="o",
+      points(plot_data$year, plot_data$log_lambda, type="o",
            ylab="logLambda", xlab="Left-out year", col="black")
     }
-    points(plot_null ~ plot_data$year, type="o", col="red", lty = p)
-    points(plot_pred ~ plot_data$year, type="o", col="blue", lty = p)
+    points(plot_data$year, plot_data$pred_null, type="o", col="red", lty = p)
+    points(plot_data$year, plot_data$pred_lm, type="o", col="brown", lty = p)
+    points(plot_data$year, plot_data$pred_spline, type="o", col="blue", lty = p)
     
   }
   
-  legend("topright", c("Data", "NULL", "crossval"),
-         col=c("black", "red", "blue"),
+  legend("topright", c("Data", "NULL", "LM", "Spline"),
+         col=c("black", "red", "brown", "blue"),
          lty = c(1, 1, 1), cex = 0.8, lwd = 2 , bty="n")
   
   # legends deviance
   dev.0=round(dev0,5)
   dev.1=round(dev1,5)
+  dev.2=round(dev2,5)
   
   legend("bottomleft", 
-         c(paste0("NULL mse: ",dev.0), paste0("Full. Mod. mse: ",dev.1)), 
+         c(paste0("NULL mse: ",dev.0), 
+           paste0("LM           : ",dev.1), 
+           paste0("Spline       : ",dev.2)),
          col=c( rep("black",length(pops)+1), "red", "blue"),
          lty = c(seq_along(pops), 1, 1, 1), bty="n", cex = 0.8)
   
@@ -185,6 +208,7 @@ for(ii in 1:length(spp_list)){
   mod_sum_l[[ii]] <- crxval_df %>%
                         mutate( dev0 = dev0,
                                 dev1 = dev1,
+                                dev2 = dev2,
                                 species = spp_name )
                     
 }
