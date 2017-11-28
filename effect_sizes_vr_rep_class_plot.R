cd7414a1e59aeb9a2850fd16c46b45f3adff1be6
Compagnoni <ac22qawo@idiv.de>
2017-11-28 17:51:06 +0100

    Correct clim_sens formula; clarify code

--- effect_sizes_vr_rep_class_plot.R
+++ effect_sizes_vr_rep_class_plot.R
@@ -21,38 +21,7 @@ sens_df       <- read.csv("allCOMPADRE_COMADREOutput.csv") %>%
                     summarize_all( mean, na.rm=T ) %>%
                     subset( !(is.nan(surv) & is.nan(grow) & is.nan(fec)) ) %>%
                     gather(response, sensitivity, -species )
-# 
-# # create each files' path 
-# path_create <- function(x,y){
-#   post_files <- Filter(function(x) grepl("posterior",x), x)
-#   paste0(y,post_files)
-# }
-# 
-# # calculate posterior means 
-# post_means <- function(x){
-#   
-#   # extract species name
-#   spp_n     <- gsub( paste0("results/moving_windows/",response,"/[a-z]{3,6}/posterior_"),"",x)
-#   spp_name  <- gsub(".csv","",spp_n)
-#   
-#   # extract climate variable name
-#   # substitute 
-#   # 1 everything before "/posterior_"
-#   # 2 everything after  "response/"
-#   clim_var  <- gsub(paste0("^(.*?)/",response,"/|/posterior_?.*"),"",x)
-#   
-#   posterior <- data.table::fread( paste0(x) ) %>%
-#     group_by( model ) %>%
-#     summarise_all( mean ) %>%
-#     ungroup %>%
-#     dplyr::select( -grep("log_lik_",names(.)) ) %>%
-#     as.data.frame %>%
-#     mutate(species = spp_name,
-#            clim_var = clim_var )
-#   
-# }
-# 
-# 
+
 # create the path of each summary file
 path_summaries <- function(x,y){
   post_files <- Filter(function(x) grepl("mod_summaries",x), x)
@@ -76,7 +45,7 @@ grab_summ_means <- function(x, response){
   mean_by_spp <- read.csv( x ) %>%
     dplyr::select( model,alpha_mean,beta_mean ) %>%
     as.data.frame %>%
-    mutate(species = spp_name,
+    mutate(species  = spp_name,
            clim_var = clim_var )
   
 }
@@ -91,13 +60,14 @@ extract_summ_means <- function(response, clim_var, mod_name = "ctrl2" ){
   summ_paths_l  <- Map(path_summaries, raw_file_vec, wds)
   summ_paths    <- Reduce(function(...) c(...), summ_paths_l) %>% as.list
   
-  # actually get
+  # actually get summary means
   means_l       <- lapply(summ_paths, grab_summ_means, response)
   means_out     <- Reduce(function(...) bind_rows(...), means_l) %>%
                       mutate( response = response) %>%
                       subset( model    == mod_name) %>%
-                      rename_( .dots = setNames("beta_mean", paste0("beta_",response)) ) %>%
-                      dplyr::select( -alpha_mean, -response)
+                      rename_( .dots = setNames("beta_mean",  paste0("beta_",response)) ) %>%
+                      rename_( .dots = setNames("alpha_mean", paste0("alpha_",response)) ) %>%
+                      dplyr::select( -response )
   
   return(means_out)
 
@@ -105,9 +75,9 @@ extract_summ_means <- function(response, clim_var, mod_name = "ctrl2" ){
 
 # read and format beta values
 means_s_pre <- extract_summ_means("SurvSSDPreRep",  c("airt","precip") )
-means_s_rep <- extract_summ_means("SurvSSDRep",  c("airt","precip") )
+means_s_rep <- extract_summ_means("SurvSSDRep",     c("airt","precip") )
 means_g_pre <- extract_summ_means("GrowSSDPreRep",  c("airt","precip") )
-means_g_rep <- extract_summ_means("GrowSSDRep",  c("airt","precip") )
+means_g_rep <- extract_summ_means("GrowSSDRep",     c("airt","precip") )
 
 # substitute string with another string
 subs_with   <- function(x, orig, new){
@@ -117,7 +87,7 @@ subs_with   <- function(x, orig, new){
 }
 
 # put betas all in one data frame
-means_all   <- Reduce( function(...) full_join(...), 
+means_beta   <- Reduce( function(...) full_join(...), 
                        list(means_s_pre, means_s_rep,
                             means_g_pre, means_g_rep) ) %>%
                   dplyr::select(species, clim_var, 
@@ -128,6 +98,22 @@ means_all   <- Reduce( function(...) full_join(...),
                   mutate( response = subs_with(response, "beta_SurvSSDRep", "survRep") ) %>%
                   mutate( response = subs_with(response, "beta_GrowSSDPreRep", "growPreRep") ) %>%
                   mutate( response = subs_with(response, "beta_GrowSSDRep", "growRep") ) 
+
+# put betas all in one data frame
+means_alph    <- Reduce( function(...) full_join(...), 
+                       list(means_s_pre, means_s_rep,
+                            means_g_pre, means_g_rep) ) %>%
+                  dplyr::select(species, clim_var, 
+                                alpha_SurvSSDPreRep, alpha_SurvSSDRep, 
+                                alpha_GrowSSDPreRep, alpha_GrowSSDRep ) %>%
+                  gather( response, alpha, -species, -clim_var ) %>%
+                  mutate( response = subs_with(response, "alpha_SurvSSDPreRep", "survPreRep") ) %>%
+                  mutate( response = subs_with(response, "alpha_SurvSSDRep", "survRep") ) %>%
+                  mutate( response = subs_with(response, "alpha_GrowSSDPreRep", "growPreRep") ) %>%
+                  mutate( response = subs_with(response, "alpha_GrowSSDRep", "growRep") ) 
+
+# means together
+means_all    <- full_join(means_alph, means_beta)
 
 # sensitivities 
 sens_ssd       <- read.csv("allCOMPADRE_COMADREOutput.csv") %>%
@@ -147,16 +133,20 @@ sens_ssd       <- read.csv("allCOMPADRE_COMADREOutput.csv") %>%
 
 # standardized betas
 means_sens     <- inner_join(means_all, sens_ssd) %>%
-                    mutate( beta_st     = beta * sensitivity,
-                            beta_st_abs = abs(beta * sensitivity) ) %>%
-                    dplyr::select(-beta,-beta_st,-sensitivity) %>%
-                    spread(species, beta_st_abs)
+                    mutate( clim_sens        = ( beta * exp(alpha) ) / ( (exp(alpha) +1)^2 ) ) %>%
+                    mutate( clim_sens        = abs(clim_sens) ) %>%
+                    mutate( clim_sens_scaled = sensitivity * clim_sens) %>%
+                    dplyr::select(-alpha,-beta,-sensitivity,-clim_sens) %>%
+                    spread(species, clim_sens_scaled)
 
+
+# plot -----------------------------------------------------------------------------------------------
+
+# separate by weather predictor
 mean_airt      <- subset(means_sens, clim_var == "airt") %>% .[c(3,1,4,2),]
 mean_prec      <- subset(means_sens, clim_var == "precip") %>% .[c(3,1,4,2),]
 
-
-# color coding for 
+# set up color coding 
 col_df <- data.frame( species = names(mean_airt)[-c(1,2)]) %>%
             left_join( spp_cat ) %>%
             mutate( col = "blue") %>%
@@ -172,24 +162,16 @@ tiff("results/moving_windows/sens_by_repr_vr.tiff",
 
 par( mfrow = c(2,1), mar = c(3,4,0.1,1.5), mgp = c(2,0.7,0) )
 
-matplot( mean_airt[,-c(1,2)], cex.lab=1.1, ylim = c(0,6), lwd = 2,
+matplot( mean_airt[,-c(1,2)], cex.lab=1.1, lwd = 2, ylim = c(0,1),
          col = col_df$col, lty = 1, type = "l", xaxt = "n", 
          ylab = expression("Sensitivity of  "*lambda*"  to temperature") )
 axis(1, at = c(1,2,3,4),
      labels = c("Surv_Pre","Grow_Pre","Surv_Rep","Grow_Rep") )
 
-matplot( mean_prec[,-c(1,2)], cex.lab=1.1, ylim = c(0,6), lwd = 2,
+matplot( mean_prec[,-c(1,2)], cex.lab=1.1, lwd = 2, ylim = c(0,1),
          col = col_df$col, lty = 1, type = "l", xaxt = "n",
          ylab = expression("Sensitivity of "*lambda*" to precipitation") )
 axis(1, at = c(1,2,3,4), 
      labels = c("Surv_Pre","Grow_Pre","Surv_Rep","Grow_Rep") )
 
 dev.off()
-
-
-means_g   <- extract_summ_means("grow", c("airt","precip") )
-means_s   <- extract_summ_means("surv", c("airt","precip") )
-means_f   <- extract_summ_means("fec",        c("airt","precip"))
-means_ll  <- extract_summ_means("log_lambda", c("airt","precip"))
-means_t   <- extract_summ_means("react_fsa",  c("airt","precip"))
-means_rho <- extract_summ_means("rho",  c("airt","precip"))
